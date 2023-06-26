ecae451c0ffcc87cee57791888a058d1f2a3db74
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2023-06-26 18:23:14 +0100

    Added autosuggestions

--- main.sh
+++ main.sh
@@ -68,6 +68,9 @@ if [ "$INSTALL_PROGRAMS" ]; then
     say "installing zsh-syntax-highlighting"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting > /dev/null 2>&1
 
+    say "installing zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions > /dev/null 2>&1
+
     say "installing powerlevel10k"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k > /dev/null 2>&1
 
@@ -116,6 +119,7 @@ if [ "$INSTALL_PROGRAMS" ] && [ "$INSTALL" ]; then
     vim -u ~/.vimrc -e -s +PlugInstall +qall
 
     say "restart terminal for changes to take effect"
+    say "on next terminal launch, wait for gitstatusd to finish installation"
 fi     
 
 if [ "$INSTALL_THEME" ]; then
