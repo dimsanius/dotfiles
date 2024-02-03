d17c332ca2fde0e3722a9f1c0eafb0f20cd96da1
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-02-03 19:16:07 +0000

    New location for files

--- main.sh
+++ main.sh
@@ -50,10 +50,6 @@ say "installing oh-my-zsh"
 # Preventing ZSH from autostarting and hijacking current terminal session after install
 RUNZSH="no" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-say "installing vim-plug"
-curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 say "installing zsh-syntax-highlighting"
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting >/dev/null 2>&1
 
@@ -72,8 +68,13 @@ git clone --depth=1 https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zs
 say "installing powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k >/dev/null 2>&1
 
+say "installing vim-plug"
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 say "symlinking files"
-stow -t ~ files -R
+stow -t ~ $SCRIPT_DIR/files -R
 
 say "clearing font cache"
 fc-cache -f -v >/dev/null 2>&1
