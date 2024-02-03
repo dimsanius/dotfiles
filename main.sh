c52c70d3b08fee44b913822959f4525ec45213dc
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-02-03 19:20:28 +0000

    Added cd

--- main.sh
+++ main.sh
@@ -72,10 +72,10 @@ say "installing vim-plug"
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $SCRIPT_DIR
+cd ~/.dotfiles
+
 say "symlinking files"
-stow -t ~ $SCRIPT_DIR/files -R
+stow -t ~ files -R
 
 say "clearing font cache"
 fc-cache -f -v >/dev/null 2>&1
