e930f3004232e99e2070b31db7aeb0ed61e1c583
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-02-03 19:34:03 +0000

    Added another way to prevent installing zshrc

--- main.sh
+++ main.sh
@@ -48,7 +48,7 @@ sudo apt install curl zsh vim terminator \
 
 say "installing oh-my-zsh"
 # Preventing ZSH from autostarting and hijacking current terminal session after install
-RUNZSH="no" REPLACE_RC="no" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH="no" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 
 say "installing zsh-syntax-highlighting"
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting >/dev/null 2>&1
