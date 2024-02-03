c39cb6cc93c024a32c08bb4546712fe2162e749e
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-02-03 17:52:52 +0000

    Changed location of dotfiles folder

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ if [ ! "$(which git)" ]; then
     exit 1
 fi
 
-mkdir -p ~/.misc
-git clone -q https://github.com/dimsanius/dotfiles.git ~/.misc/dotfiles
-~/.misc/dotfiles/main.sh -ipv
+git clone -q https://github.com/dimsanius/dotfiles.git ~/.dotfiles
+~/.dotfiles/main.sh -ipv
