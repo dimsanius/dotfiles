36dc0be28e924881054cc93a77b0c63a21201b55
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-02-03 22:42:26 +0000

    Added GitHub identity

--- install.sh
+++ install.sh
@@ -10,5 +10,8 @@ if [ ! "$(which git)" ]; then
     sudo apt install git -y
 fi
 
+# Not quite reliable, but this should get GitHub SSH keys for interaction-less repo clone
+ssh-keyscan -H github.com >>~/.ssh/known_hosts 2>/dev/null
+
 git clone -q git@github.com:dimsanius/dotfiles.git ~/.dotfiles
 ~/.dotfiles/main.sh -v
