262524886f0a8bfbf8f3700e5ba80662f958dbc9
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-02-04 01:51:06 +0000

    Updated echo

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ ssh-keyscan -H github.com >>~/.ssh/known_hosts 2>/dev/null
 git clone git@github.com:dimsanius/dotfiles.git ~/.dotfiles
 
 if [ $? -ne 0 ]; then
-    echo "Git clone failed. Please check your SSH keys."
+    echo "ERROR: Failed to clone a repo. Please check your SSH keys. Aborting."
     exit 1
 fi
 
