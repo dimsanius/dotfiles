b6e007d2d07fedd0eb157b1539b43b873d921cdf
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-03-04 23:45:09 +0000

    Updated notice

--- run_once_99_notice.sh
+++ run_once_99_notice.sh
@@ -4,5 +4,6 @@ say() {
     echo "==> $1"
 }
 
-say "restart terminal for changes to take effect"
-say "on next terminal launch, wait for gitstatusd to finish installation"
+for a in `seq 20`; do echo -n _; done; echo
+say "Restart terminal for changes to take effect."
+say "On next terminal launch, wait for gitstatusd to finish installation process."
