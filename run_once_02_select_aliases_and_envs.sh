b30406904ccfcd00c9745eff4ae49cce00b01110
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-03-04 20:29:17 +0000

    Removed unused block

--- run_once_02_select_aliases_and_envs.sh
+++ run_once_02_select_aliases_and_envs.sh
@@ -10,18 +10,6 @@ echo "\$HOME/.myenv/aliases_exa.sh" >> $HOME/.included_envs.sh
 echo "\$HOME/.myenv/env_zoxide.sh" >> $HOME/.included_envs.sh
 
 # Include this ocassionally
-read -p "==> Include Zephyr env and aliases? (Y/n) " response
-
-case "$response" in
-    [Yy]) 
-        say "Zephyr env and aliases will be included"
-        echo "\$HOME/.myenv/aliases_zephyr.sh" >> $HOME/.included_envs.sh
-        echo "\$HOME/.myenv/env_zephyr.sh" >> $HOME/.included_envs.sh
-    * )
-        say "Zephyr env and aliases will not be included"
-        ;;
-esac
-
 while true; do
     read -p "==> Include Zephyr env and aliases? [Y/n] " yn
     case $yn in
