b3a022cd0f8822782dc0ab6c1432b817d17b973e
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-03-04 20:46:32 +0000

    Added warning

--- run_once_02_select_aliases_and_envs.sh
+++ run_once_02_select_aliases_and_envs.sh
@@ -6,10 +6,12 @@ say() {
 
 chmod +x $HOME/.myenv/*.sh
 
+env_selector_file="$HOME/.env_selector.sh"
+
 # Include this always
-echo "source \$HOME/.myenv/aliases_batcat.sh" >> $HOME/.included_envs.sh
-echo "source \$HOME/.myenv/aliases_exa.sh" >> $HOME/.included_envs.sh
-echo "source \$HOME/.myenv/env_zoxide.sh" >> $HOME/.included_envs.sh
+echo "source \$HOME/.myenv/aliases_batcat.sh" >> $env_selector_file
+echo "source \$HOME/.myenv/aliases_exa.sh" >> $env_selector_file
+echo "source \$HOME/.myenv/env_zoxide.sh" >> $env_selector_file
 
 # Include this ocassionally
 while true; do
@@ -17,8 +19,8 @@ while true; do
     case $yn in
         [Yy]* ) 
             say "Zephyr env and aliases will be included"
-            echo "source \$HOME/.myenv/aliases_zephyr.sh" >> $HOME/.included_envs.sh
-            echo "source \$HOME/.myenv/env_zephyr.sh" >> $HOME/.included_envs.sh
+            echo "source \$HOME/.myenv/aliases_zephyr.sh" >> $env_selector_file
+            echo "source \$HOME/.myenv/env_zephyr.sh" >> $env_selector_file
             break
             ;;
         [Nn]* ) 
