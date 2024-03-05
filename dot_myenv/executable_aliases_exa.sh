ee87df9173b92aca185e2b547d715810bfb805a2
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-03-05 08:26:16 +0000

    Changed tree

--- dot_myenv/executable_aliases_exa.sh
+++ dot_myenv/executable_aliases_exa.sh
@@ -3,3 +3,5 @@
 alias la="exa --icons --long --group-directories-first --group --all --git"
 alias ll="exa --icons --long --group-directories-first --group --git"
 alias ls="exa --icons --group-directories-first --group"
+
+alias tree="exa --tree"
