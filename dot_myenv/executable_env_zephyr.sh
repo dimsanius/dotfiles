9f676fa149e3c780a6b70a03fdfe8f77fb5e225d
Dmitrij Santarovic <ds@ferarudynamics.co.uk>
2024-05-20 10:42:06 +0100

    Added older gnu toolchain for zephyr

--- dot_myenv/executable_env_zephyr.sh
+++ dot_myenv/executable_env_zephyr.sh
@@ -9,6 +9,10 @@ export ZEPHYR_BASE=$HOME/dev/hav-aegis-tools/zephyrproject/zephyr
 # export ZEPHYR_SDK_INSTALL_DIR=$HOME/dev/hav-aegis-tools/zephyr-sdk-0.15.2
 
 
-# ARM Toolchain
+# ARM Toolchain (newer, not used by CH)
+# export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
+# export GNUARMEMB_TOOLCHAIN_PATH=$HOME/dev/hav-aegis-tools/arm-gnu-toolchain-12.2.mpacbti-rel1-x86_64-arm-none-eabi
+
+# ARM Toolchain (older, used by CH)
 export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
-export GNUARMEMB_TOOLCHAIN_PATH=$HOME/dev/hav-aegis-tools/arm-gnu-toolchain-12.2.mpacbti-rel1-x86_64-arm-none-eabi
+export GNUARMEMB_TOOLCHAIN_PATH=$HOME/dev/hav-aegis-tools/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi
