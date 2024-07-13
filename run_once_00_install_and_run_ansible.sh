5640eae419ee2127571cd556a8c65ab4f3c8b99b
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-07-13 14:23:51 +0100

    Updated the ansible retry logic

--- run_once_00_install_and_run_ansible.sh
+++ run_once_00_install_and_run_ansible.sh
@@ -9,8 +9,27 @@ python3 -m venv ~/.bootstrap/.venv --upgrade-deps
 source ~/.bootstrap/.venv/bin/activate
 pip install -q ansible
 
-say "Running Ansible..."
-ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass --ask-vault-pass
-say "Ansible completed"
+retries=1
+while [ "$retries" -le 3 ]; do
+
+    say "[try $retries of 3] Running Ansible..."
+    if ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass --ask-vault-pass; then
+        # Command finished successfully
+        say "Ansible completed successfully"
+        break
+    else
+        return_code=$?
+        if [ "$return_code" -eq 99 ]; then
+            say "User interrupted. Aborting install."
+            break
+        fi
+        # Failed for whatever other reason. Try to retry.
+        retries=$((retries + 1))
+    fi
+done
+
+if [ "$retries" -eq 4 ]; then
+    say "Ansible failed"
+fi
 
 deactivate
