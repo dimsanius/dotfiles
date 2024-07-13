118344c6bf49aaccff6eda7f16cadb6143d6dd4f
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-07-13 13:52:18 +0100

    Added vault pass ask

--- run_once_00_install_and_run_ansible.sh
+++ run_once_00_install_and_run_ansible.sh
@@ -10,7 +10,7 @@ source ~/.bootstrap/.venv/bin/activate
 pip install -q ansible
 
 say "Running Ansible..."
-ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
+ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass --ask-vault-pass
 say "Ansible completed"
 
 deactivate
