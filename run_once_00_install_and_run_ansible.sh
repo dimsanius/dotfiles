6ccf3dfaeadd5b3b71993218d5acd76bd07fa01f
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2024-07-13 14:42:04 +0100

    Adjusted conditions for ansible

--- run_once_00_install_and_run_ansible.sh
+++ run_once_00_install_and_run_ansible.sh
@@ -5,7 +5,7 @@ say() {
 }
 
 say "Creating python venv and installing ansible"
-python3 -m venv ~/.bootstrap/.venv --upgrade-deps
+python3 -m venv ~/.bootstrap/.venv
 source ~/.bootstrap/.venv/bin/activate
 pip install -q ansible
 
@@ -15,21 +15,16 @@ while [ "$retries" -le 3 ]; do
     say "[try $retries of 3] Running Ansible..."
     if ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass --ask-vault-pass; then
         # Command finished successfully
-        say "Ansible completed successfully"
+        say "Ansible compeleted successfully."
         break
     else
-        return_code=$?
-        if [ "$return_code" -eq 99 ]; then
-            say "User interrupted. Aborting install."
-            break
-        fi
         # Failed for whatever other reason. Try to retry.
         retries=$((retries + 1))
     fi
 done
 
 if [ "$retries" -eq 4 ]; then
-    say "Ansible failed"
+    say "Ansible failed to complete."
 fi
 
 deactivate
