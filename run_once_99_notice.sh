47a56a440ef1692b730c85aee3f643592e602a42
Dmitrij Santarovic <ds@ferarudynamics.co.uk>
2024-07-15 14:12:08 +0100

    Updated prompt

--- run_once_99_notice.sh
+++ run_once_99_notice.sh
@@ -5,5 +5,5 @@ say() {
 }
 
 for a in `seq 20`; do echo -n _; done; echo
-say "Restart terminal for changes to take effect."
+say "Restart terminal and re-login into your system for changes to take effect."
 say "On next terminal launch, wait for powerlevel10k to fetch gitstatusd."
