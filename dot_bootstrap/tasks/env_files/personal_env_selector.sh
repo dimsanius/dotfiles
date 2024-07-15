75122d8176a3e95c181a2fd2861eef65ef8efe58
Dmitrij Santarovic <ds@ferarudynamics.co.uk>
2024-07-15 17:07:36 +0100

    Added work/personal env selector

---
+++ dot_bootstrap/tasks/env_files/personal_env_selector.sh
@@ -0,0 +1,7 @@
+source \$HOME/.myenv/aliases_batcat.sh
+source \$HOME/.myenv/aliases_eza.sh
+source \$HOME/.myenv/env_zoxide.sh
+source \$HOME/.myenv/env_chezmoi.sh
+source \$HOME/.myenv/env_fzf.sh
+source \$HOME/.myenv/env_fzf_completion.zsh
+source \$HOME/.myenv/env_fzf_key_bindings.zsh
