857611b228b217bc470a0aed4a3d83301aa53780
Dmitrij Santarovic <ds@ferarudynamics.co.uk>
2024-07-15 16:03:07 +0100

    Added environment split and added slack

--- run_once_00_install_and_run_ansible.sh
+++ run_once_00_install_and_run_ansible.sh
@@ -4,7 +4,32 @@ say() {
     echo "==> $1"
 }
 
-say "Creating python venv and installing ansible"
+while true
+do
+    say "Environment selection:"
+    echo " 1. Personal"
+    echo " 2. Work"
+    echo ""
+    read -p " Selection [1-2]: " -n 1 answer
+    
+    if [[ "$answer" == 1 ]];then
+        environment="personal"
+        break
+    fi
+
+    if [[ "$answer" == 2 ]];then
+        environment="work"
+        break
+    fi
+
+    echo ""
+    echo "Wrong selection. Try again."
+done
+
+echo $environment
+
+exit
+say "Preparing Python's virtual environment"
 python3 -m venv ~/.bootstrap/.venv
 source ~/.bootstrap/.venv/bin/activate
 pip install -q ansible gnome-extensions-cli
@@ -13,7 +38,7 @@ retries=1
 while [ "$retries" -le 3 ]; do
 
     say "[try $retries of 3] Running Ansible..."
-    if ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass --ask-vault-pass; then
+    if ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass --ask-vault-pass --extra-vars "environment=$environment"; then
         # Ansible run succeeded
         say "Ansible compeleted successfully."
         break
