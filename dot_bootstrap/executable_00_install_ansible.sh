9ec2dae69506f277bbfa00aa8f289a717f8c0628
Dmitrij Santarovic <ds@ferarudynamics.co.uk>
2024-08-05 09:56:19 +0100

    Added verbosity

--- dot_bootstrap/executable_00_install_ansible.sh
+++ dot_bootstrap/executable_00_install_ansible.sh
@@ -4,9 +4,16 @@ say() {
     echo "[Ansible Install] ==> $1"
 }
 
-say "Creating Python's virtual environment"
+say "Checking Ansible status..."
+
+if [ -d "~/.bootstrap/.venv" ]; then
+    say "Ansible found. Deleting it."
+    rm -r venv ~/.bootstrap/.venv
+fi
+
+say "Installing Ansible"
 python3 -m venv ~/.bootstrap/.venv
 source ~/.bootstrap/.venv/bin/activate
 
-say "Installing pip dependencies"
+say "Installing Ansible dependencies"
 pip install -q -r ~/.bootstrap/requirements.txt
