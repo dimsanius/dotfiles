ac670a3666b1bf5e6cbebc5e537e000fdce9aacd
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-04 00:29:46 +0100

    Added uv usage

--- home/dot_bootstrap/executable_00_install_ansible.sh
+++ home/dot_bootstrap/executable_00_install_ansible.sh
@@ -4,16 +4,16 @@ say() {
     echo "[Ansible Install] ==> $1"
 }
 
-say "Checking Ansible status..."
+say "Checking venv status..."
 
 if [ -d "$HOME/.bootstrap/.venv" ]; then
-    say "Ansible found. Deleting it."
+    say "venv found. Deleting it."
     rm -r $HOME/.bootstrap/.venv
 fi
 
-say "Installing Ansible"
-python3 -m venv $HOME/.bootstrap/.venv
+say "Creating venv"
+uv venv $HOME/.bootstrap/.venv
 source $HOME/.bootstrap/.venv/bin/activate
 
 say "Installing Ansible dependencies"
-pip install -q -r $HOME/.bootstrap/requirements.txt
+uv pip install -q -r $HOME/.bootstrap/requirements.txt
