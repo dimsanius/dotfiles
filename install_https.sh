7edc6fa17d04faa4ab2cc4d774806c88804b4577
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-03 23:47:25 +0100

    Fixed https to actually use https

--- install_https.sh
+++ install_https.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt install -y python3-venv python3-apt git
 
-sh -c "$(wget -q0- get.chezmoi.io/lb)" -- init --apply git@github.com:dimsanius/dotfiles.git
+wget -qO- https://get.chezmoi.io/lb | sh -s -- init --apply https://github.com/dimsanius/dotfiles.git
