637f7ae55fd2e6c53da01f6ea12b6ec51372aac0
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-03 23:51:00 +0100

    Added uv installs

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt install -y python3-venv python3-apt git
+set -e
 
+sudo apt update
+sudo apt install -y \
+            python3-venv \
+            python3-apt \
+            git
+
+wget -qO- https://astral.sh/uv/install.sh | sh
 wget -qO- https://get.chezmoi.io/lb | sh -s -- init --apply git@github.com:dimsanius/dotfiles.git
