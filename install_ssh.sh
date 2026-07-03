3677a32c44626354764b13bd08105030a4d5d4fd
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-03 23:36:43 +0100

    Removed curl dependency

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y python3-venv python3-apt curl git
+sudo apt install -y python3-venv python3-apt git
 
 sh -c "$(wget -q0- get.chezmoi.io/lb)" -- init --apply git@github.com:dimsanius/dotfiles.git
