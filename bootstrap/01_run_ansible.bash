52f84a14868d68c8a0d3a75bba0005b3911e8d48
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-04 16:15:52 +0100

    Removed unused prompts

--- bootstrap/01_run_ansible.bash
+++ bootstrap/01_run_ansible.bash
@@ -6,33 +6,11 @@ say() {
 
 source $HOME/.local/share/chezmoi/bootstrap/.venv/bin/activate
 
-while true; do
-    say "Environment selection:"
-    echo " 1. Personal"
-    echo " 2. Work"
-    echo ""
-    read -p " Selection [1-2]: " -n 1 answer < /dev/tty
-    echo ""
-    echo ""
-
-    if [[ "$answer" == 1 ]]; then
-        environment="personal"
-        break
-    fi
-
-    if [[ "$answer" == 2 ]]; then
-        environment="work"
-        break
-    fi
-
-    echo "Wrong selection. Try again."
-done
-
 retries=1
 while [ "$retries" -le 3 ]; do
 
     say "[try $retries of 3] Running Ansible..."
-    if ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook $HOME/.local/share/chezmoi/bootstrap/setup.yml --ask-become-pass --ask-vault-pass --extra-vars "environment_type=$environment"; then
+    if ANSIBLE_LOCALHOST_WARNING=False ANSIBLE_INVENTORY_UNPARSED_WARNING=False ansible-playbook $HOME/.local/share/chezmoi/bootstrap/setup.yml --ask-become-pass; then
         # Ansible run succeeded
         say "Ansible compeleted successfully."
         break
