a886b6aed672e2cfd5c0c5255b5f5319e9a70645
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-04 17:24:33 +0100

    Fixed double logging

--- bootstrap/00_install_ansible.bash
+++ bootstrap/00_install_ansible.bash
@@ -3,9 +3,7 @@
 log() { echo "→ $*" >&2; }
 run() { log "$*"; "$@"; }
 
-log "(Re-)Creating venv"
 run uv venv --clear $HOME/.local/share/chezmoi/bootstrap/.venv
 run source $HOME/.local/share/chezmoi/bootstrap/.venv/bin/activate
 
-log "Installing Ansible dependencies"
 run uv pip install -r $HOME/.local/share/chezmoi/bootstrap/requirements.txt
