b96f5845493a6c81a200bfbd6a24ab32da351a97
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-05 17:55:27 +0100

    Updated exit handling

--- bootstrap/run_ansible.bash
+++ bootstrap/run_ansible.bash
@@ -5,17 +5,11 @@ set -euo pipefail
 log() { echo "→ $*" >&2; }
 run() { log "$*"; "$@"; }
 
-die() {
-    local status="${1:-1}"
-
-    # If sourced, return; otherwise exit.
-    return "$status" 2>/dev/null || exit "$status"
-}
-
 BOOTSTRAP_DIR="$HOME/.local/share/chezmoi/bootstrap"
 ANSIBLE_PLAYBOOK="$BOOTSTRAP_DIR/setup.yml"
 
 max_attempts=3
+success=0
 
 for current_attempt in $(seq 1 "$max_attempts"); do
     log "[attempt $current_attempt of $max_attempts] Running Ansible..."
@@ -29,12 +23,14 @@ for current_attempt in $(seq 1 "$max_attempts"); do
         --ask-become-pass; then
         # Ansible run succeeded
         log "[attempt $current_attempt of $max_attempts] Ansible completed successfully"
-        die 0
+        success=1
+        break
     else
         # Ansible run attempt failed
         log "[attempt $current_attempt of $max_attempts] Attempt $current_attempt failed."
     fi
 done
 
-log "All $max_attempts Ansible run attempts failed. Aborting."
-die 1
+if [ "$success" -eq 0 ];then
+    log "All $max_attempts Ansible run attempts failed. Aborting."
+fi
