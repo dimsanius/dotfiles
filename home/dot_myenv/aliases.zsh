d01cbaa8a461251c612b38c63fa7d31e5abd33a1
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-18 11:35:07 +0100

    Added templated functions

--- home/dot_myenv/aliases.zsh
+++
@@ -1,8 +0,0 @@
-# --- batcat ---
-alias cat="batcat"
-
-# --- eza ---
-alias la="eza --icons --long --group-directories-first --group --all --git"
-alias ll="eza --icons --long --group-directories-first --group --git"
-alias ls="eza --icons --group-directories-first --group"
-alias tree="eza --tree"
