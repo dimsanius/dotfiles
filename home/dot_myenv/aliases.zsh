2e1fcc7a33735cb8dbbd1b164d266ff7e07193b7
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-18 11:26:23 +0100

    Extracted itesm from zshrc

---
+++ home/dot_myenv/aliases.zsh
@@ -0,0 +1,8 @@
+# --- batcat ---
+alias cat="batcat"
+
+# --- eza ---
+alias la="eza --icons --long --group-directories-first --group --all --git"
+alias ll="eza --icons --long --group-directories-first --group --git"
+alias ls="eza --icons --group-directories-first --group"
+alias tree="eza --tree"
