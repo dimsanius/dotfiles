71cd58ef6151625ca5ef51ddb9c97b49e2e9ad7e
Dmitrij Santarovic <dmitrij.santarovic@gmail.com>
2026-07-18 11:30:08 +0100

    Added functions

--- home/dot_myenv/aliases_eza.sh
+++
@@ -1,5 +0,0 @@
-alias la="eza --icons --long --group-directories-first --group --all --git"
-alias ll="eza --icons --long --group-directories-first --group --git"
-alias ls="eza --icons --group-directories-first --group"
-
-alias tree="eza --tree"
